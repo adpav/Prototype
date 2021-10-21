674bb824813c88a236b19deffd9b3518f81128b2
Adam Hosker-Williams <88383419+adpav@users.noreply.github.com>
2021-10-21 11:41:46 +0100

    Add files via upload

---
+++ myscripts.js
@@ -0,0 +1,39 @@
+
+
+// $('size-selector_size').on('click', function(){
+//     $('size-selector_size').removeClass('size-selector_size:active');
+//     $(this).addClass('size-selector_size:active');
+// });
+
+
+
+
+/////////////////////  M O D A L  ///////////////////// 
+
+
+// Get DOM Elements
+const modal = document.querySelector('#my-modal');
+const modalBtn = document.querySelector('#modal-btn');
+const closeBtn = document.querySelector('.close');
+
+// Events
+modalBtn.addEventListener('click', openModal);
+closeBtn.addEventListener('click', closeModal);
+window.addEventListener('click', outsideClick);
+
+// Open
+function openModal() {
+  modal.style.display = 'block';
+}
+
+// Close
+function closeModal() {
+  modal.style.display = 'none';
+}
+
+// Close If Outside Click
+function outsideClick(e) {
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
+}
